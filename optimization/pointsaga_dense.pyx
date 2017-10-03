fa7f103a1096e4e76102bba2f96890861f3342b5
Aaron Defazio <adefazio@fb.com>
2017-10-03 11:11:10 -0400

    unsparsified

--- optimization/pointsaga_dense.pyx
+++ optimization/pointsaga_dense.pyx
@@ -52,51 +52,20 @@ def pointsaga_dense(A, double[:] b, props):
 
     loss = getLoss(A, b, props)
 
-    cdef double wscale = 1.0 # Scaling factor for xk.
     cdef double ry
 
     np.random.seed(42)
 
-    logger = logging.getLogger("pointsaga2")
+    logger = logging.getLogger("pointsaga dense")
 
     cdef int maxiter = props.get("passes", 10)
-
-    # Tracks for each entry of x, what iteration it was last updated at.
-    cdef unsigned int[:] lag = np.zeros(m, dtype='I')
 
     # Used to convert from prox_f to prox of f plus a regulariser.
     cdef double gamma_prime
     cdef double prox_conversion_factor = 1-(reg*gamma)/(1+reg*gamma)
 
-    # This is just a table of the sum the geometric series (1-reg*gamma)
-    # It is used to correctly do the just-in-time updating when
-    # L2 regularisation is used.
-    cdef double[:] lag_scaling = np.zeros(n+2)
-    lag_scaling[0] = 0.0
-    lag_scaling[1] = 1
-    cdef double geosum = 1
-    cdef double mult = prox_conversion_factor
-    for i in range(2,n+2):
-        geosum *= mult
-        lag_scaling[i] = lag_scaling[i-1] + geosum
-
-    # For linear learners, we only need to store a single
-    # double for each data point, rather than a full gradient vector.
-    # The value stored is the activation * wscale * x product.
+    # gradient table
     cdef double[:] c = np.zeros(n)
-
-    if False:
-      for i in range(n):
-          indstart = indptr[i]
-          indend = indptr[i+1]
-          ydata = data[indstart:indend]
-          yindices = indices[indstart:indend]
-          ylen = indend-indstart
-          ry = b[i]
-
-          c[i] = loss.subgradient(i, 0.0)
-
-          add_weighted(gk, ydata, yindices, ylen, c[i]/n)
 
     cdef unsigned int k = 0 # Current iteration number
 
@@ -138,42 +107,22 @@ def pointsaga_dense(A, double[:] b, props):
 
             gamma_prime = gamma*prox_conversion_factor
 
-            if use_lag:
-              lagged_update(k, xk, gk, lag, yindices, ylen, lag_scaling, -gamma/wscale)
+            #Apply old gradient from table
+            add_weighted(xk, ydata, yindices, ylen, c[i]*gamma)
 
-              add_weighted(xk, ydata, yindices, ylen, c[i]*gamma/wscale)
+            for p in range(m):
+                xk[p] = xk[p] - gamma*gk[p]
+                xk[p] = prox_conversion_factor*xk[p]
 
-              wscale *= prox_conversion_factor
-            else:
-              #Apply gk (gradient average quantity)
-              for p in range(m):
-                  xk[p] = xk[p] - gamma*gk[p]
-
-              # Apply the old g_i
-              add_weighted(xk, ydata, yindices, ylen, c[i]*gamma)
-
-              # Scale xk
-              for p in range(m):
-                  xk[p] = prox_conversion_factor*xk[p]
-
-            activation = wscale * spdot(xk, ydata, yindices, ylen)
+            activation = spdot(xk, ydata, yindices, ylen)
             (new_loc, cnew) = loss.prox(gamma_prime, i, activation)
 
             cold = c[i]
             cchange = cnew-c[i]
             c[i] = cnew
 
-            # Double check prox
-            if True and loss.is_differentiable:
-              sg = loss.subgradient(i, new_loc)
-              #sg_atc =
-              if fabs(sg - cnew) > 1e-5:
-                logger.info("Bad prox. sg: %1.8e, prox-sg: %1.8e, sg2: %1.8e, sg3: %1.8e",
-                            sg, cnew, sg/gamma_prime, sg*gamma_prime)
-
-
-            # Update xk with the f_j^prime(phi_j) (with wscale scaling)
-            add_weighted(xk, ydata, yindices, ylen, -cnew*gamma_prime/wscale)
+            # Update xk with the f_j^prime(phi_j)
+            add_weighted(xk, ydata, yindices, ylen, -cnew*gamma_prime)
 
             # update the gradient average
             add_weighted(gk, ydata, yindices, ylen, cchange/n)
@@ -183,17 +132,6 @@ def pointsaga_dense(A, double[:] b, props):
 
         logger.info("Epoch %d finished", epoch)
 
-        # Unlag the vector
-        if use_lag:
-          gscaling = -gamma/wscale
-          for ind in range(m):
-            lagged_amount = k-lag[ind]
-            if lagged_amount > 0:
-              lag[ind] = k
-              xk[ind] += (lag_scaling[lagged_amount+1]-1)*gscaling*gk[ind]
-            xk[ind] = wscale*xk[ind]
-
-          wscale = 1.0
 
         loss.store_training_loss(xk)
 
